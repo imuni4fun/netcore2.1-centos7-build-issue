032c91ef01fc86ad5957d53ed1f98fd6a65c98a2
Jason Keller <solutions@jasonckeller.com>
2018-08-01 22:37:14 -0400

    capturing all artifacts of failed build attempt

---
+++ should-build/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace should_build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
